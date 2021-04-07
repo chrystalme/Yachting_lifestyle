df01dd45224ee36e1188c9bd1c104941e255971d
chrystalme <afam_ifeanyi@live.com>
2021-04-07 19:26:46 +0300

    Add sessions_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,33 @@ class UsersController < ApplicationController
     @users = User.all
   end
   def new
-    user = User.create!(user_params)
-    redirect_to root_path
+    user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "#{@user.name} has been created successfully."
+      redirect_to root_path   
+    else
+      render :new
+    end
+  end
+  def show
+    @user = user.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def edit
+    
+  end
+  def destroy
+    
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name)
   end
