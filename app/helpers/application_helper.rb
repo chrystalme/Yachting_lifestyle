477c81dde574ed3dec32b4770803cbc5a4a33bfc
chrystalme <afam_ifeanyi@live.com>
2021-04-08 16:58:37 +0300

    Add basic header and links/

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,25 @@ module ApplicationHelper
     end
     output.html_safe
   end
+
+  def show_errors(item)
+    output = ''
+    if item.errors.any?
+      item.errors.full_messages.each do |msg|
+        output << '<div>#{msg}</div>'
+      end
+    end
+    output.html_safe
+  end
+
+  def show_header
+    output = ''
+    output << if logged_in?
+      "<a href='#'> Welcome #{current_user.name}</a> | #{link_to 'Log Out', log_out_path, method: :delete
+      }"
+    else
+      "#{link_to 'Register', sign_up_path} | #{link_to 'Login', log_in_path}"
+    end
+    output.html_safe
+  end
 end
