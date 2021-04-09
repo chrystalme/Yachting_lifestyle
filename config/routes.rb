e626e6c92543d41ae1148fb6bccec382ef836b54
chrystalme <afam_ifeanyi@live.com>
2021-04-09 11:15:22 +0300

    Add article controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'sign_up', to: 'users#new', as: 'sign_up'
