c94d42755b40b6c2fd19082fa9359a4d9f2fad5e
chrystalme <afam_ifeanyi@live.com>
2021-04-09 17:48:08 +0300

    Finish login

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
-  has_and_belongs_to_many :categories
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   has_one_attached :image
