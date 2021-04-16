03ba5d5aacbc0becb3fb0d5531f11851f3764f21
chrystalme <afam_ifeanyi@live.com>
2021-04-16 13:43:46 +0300

    Move logic to helper files

---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,26 @@
+module ArticlesHelper
+  def show_articles
+    output = ''
+    @articles.each do |article|
+   output << "<div class='row g-0'>
+      <div class='col-3'>"
+        if article.image.attached? 
+        output << "#{link_to(image_tag(article.image.variant(resize_to_limit: [300, 300])), article_path(article))}
+      </div>
+      <div class='col-3'>
+        <div class='card-body'>
+          <h5 class='card-title'> #{article.title}</h5>
+          <p class='card-text text-black'>
+             #{article.text.truncate_words(7)}
+          </p>
+          #{show_vote_btn(article)}<em class='badge bg-primary mx-3'>#{article.votes.count.to_s + ' Vote(s)'} </em>
+          <h6 class='card-text mt-2'><strong>Author: </strong>#{article.author.name} </h6>
+        </div>
+      </div>
+      "
+      end
+      output << "</div>"
+    end
+    output.html_safe
+  end
+end
