f4c81696da9a1b65960af55fc1465be6299c002e
chrystalme <afam_ifeanyi@live.com>
2021-04-16 15:32:25 +0300

    Fix rubocop errors before deploy

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,9 +2,9 @@ module ArticlesHelper
   def show_articles
     output = ''
     @articles.each do |article|
-   output << "<div class='row g-0'>
+      output << "<div class='row g-0'>
       <div class='col-3'>"
-        if article.image.attached? 
+      if article.image.attached?
         output << "#{link_to(image_tag(article.image.variant(resize_to_limit: [300, 300])), article_path(article))}
       </div>
       <div class='col-3'>
@@ -13,13 +13,13 @@ module ArticlesHelper
           <p class='card-text text-black'>
              #{article.text.truncate_words(7)}
           </p>
-          #{show_vote_btn(article)}<em class='badge bg-primary mx-3'>#{article.votes.count.to_s + ' Vote(s)'} </em>
+          #{show_vote_btn(article)}<em class='badge bg-primary mx-3'>#{"#{article.votes.count} Vote(s)"} </em>
           <h6 class='card-text mt-2'><strong>Author: </strong>#{article.author.name} </h6>
         </div>
       </div>
       "
       end
-      output << "</div>"
+      output << '</div>'
     end
     output.html_safe
   end
