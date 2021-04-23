1162daa1b9a603077fd621c3c525edf5e191c19f
chrystalme <afam_ifeanyi@live.com>
2021-04-23 11:14:01 +0300

    Prepared for deploy

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   def show_featured
     output = ''
     output << if @featured_article.present?
-                "#{image_tag(@featured_article.first.image.variant(resize_to_limit: [1000, 300]))}
+                "#{cl_image_tag(@featured_article.first.image.key, width: 1000, crop: :scale)}
       <div class='feature-sub'><h6><strong>#{link_to(@featured_article.first.title, article_path(@featured_article.first))}</strong></h6>
       <p> #{@featured_article.first.text.truncate_words(30)}</p></div>"
               else
@@ -18,7 +18,7 @@ module UsersHelper
 
       output << "<div class='col-3'>
         <div class='other-article'>
-          #{link_to(image_tag(article.image.variant(resize_to_limit: [600, 400])), article_path(article))}
+          #{link_to(cl_image_tag(article.image.key, width: 600, crop: :scale), article_path(article))}
           <p>
             #{article.text.truncate_words(5)}
           </p>
